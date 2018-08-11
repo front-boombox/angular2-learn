e03c8c0d27f737f40079bb5b5f2a7b6666ab8d99
jc_dglubokiy <dglubokiy@justcoded.co>
2018-08-11 17:58:01 +0300

    services

---
+++ services/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+       
+@Component({
+    selector: 'services-app',
+    template: `<data-comp></data-comp>
+                <data-comp></data-comp>`
+})
+export class AppComponent {}
