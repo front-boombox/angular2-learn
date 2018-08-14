ad4d29ebe9aed5b96c49c31fac81ade598cea311
jc_dglubokiy <dglubokiy@justcoded.co>
2018-08-14 23:48:57 +0300

    router

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,47 +1,37 @@
 import { Component} from '@angular/core';
-         
-export class Phone{
-    constructor(public title: string, 
-                public price: number, 
-                public company: string)
-    { }
-}
- 
+import { NgForm} from '@angular/forms';
+
 @Component({
-    selector: 'forms-app',
-    template: `<div class="col-xs-8"> 
-                    <div class="form-group">
-                        <label>Название модели</label>
-                        <input class="form-control" name="title" [(ngModel)]="title" />
-                    </div>
-                    <div class="form-group">
-                        <label>Цена</label>
-                        <input type="number" class="form-control" name="price" [(ngModel)]="price" />
-                    </div>
-                    <div class="form-group">
-                        <label>Производитель</label>
-                        <select class="form-control" name="company" [(ngModel)]="company">
-                            <option  *ngFor="let comp of companies" [value]="comp">
-                                {{comp}}
-                            </option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <button class="btn btn-default" (click)="addPhone(title, price, company)">Добавить</button>
-                    </div>
-              </div>
-              <div><h3>Добавленные элементы</h3>
-                    <ul *ngFor="let p of phones">
-                        <li>{{p.title}} ({{p.company}}) - {{p.price}}</li>
-                    </ul>
-              </div>`
+    selector: 'my-app',
+	styles: [`
+		input.ng-touched.ng-invalid {border:solid red 2px;}
+		input.ng-touched.ng-valid {border:solid green 2px;}
+	`],
+    template: `<form #myForm="ngForm" novalidate (ngSubmit)="onSubmit(myForm)">
+					<div class="form-group">
+						<label>Имя</label>
+						<input class="form-control" name="name" [(ngModel)]="name" required />
+					</div>
+					<div class="form-group">
+						<label>Email</label>
+						<input class="form-control" name="email" ngModel 
+							required email />
+					</div>
+					<div class="form-group">
+						<label>Телефон</label>
+						<input class="form-control" name="phone" ngModel 
+							required pattern="[0-9]{10}" />
+					</div>
+					<div class="form-group">
+                        <input type="submit" [disabled]="myForm.invalid" class="btn btn-default" value="Отправить" />
+					</div>
+				</form>
+				<div>Имя: {{myForm.value.name}}</div>
+				<div>Email: {{myForm.value.email}}</div>`
 })
 export class AppComponent { 
- 
-    phones: Phone[] = [];
-    companies: string[] = ["Apple", "Huawei", "Xiaomi", "Samsung", "LG", "Motorola", "Alcatel"];
-     
-    addPhone(title: string, price: number, company: string){
-        this.phones.push(new Phone(title, price, company));
-    }
+
+	submit(form: NgForm){
+		console.log(form);
+	}
 }
---
+++ http/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit} from '@angular/core';
+import { HttpService} from './http.service';
+import {User} from './user';
+   
+@Component({
+    selector: 'my-app',
+    template: `<ul>
+                <li *ngFor="let user of users">
+                <p>Имя пользователя: {{user?.name}}</p>
+                <p>Возраст пользователя: {{user?.age}}</p>
+                </li>
+            </ul>`,
+    providers: [HttpService]
+})
+export class AppComponent implements OnInit { 
+   
+    users: User[]=[];
+     
+    constructor(private httpService: HttpService){}
+      
+    ngOnInit(){
+          
+        this.httpService.getData().subscribe(data => this.users=data["userList"]);
+    }
+}
